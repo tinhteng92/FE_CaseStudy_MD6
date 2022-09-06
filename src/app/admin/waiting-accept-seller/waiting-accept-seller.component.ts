f30f574ab8fa91cf66bbaac9a1e50df63e372ab0
Chu Tinh <tinhteng@gmail>
2022-09-06 12:01:24 +0700

    ghep bootstrap bang waiting

---
+++ src/app/admin/waiting-accept-seller/waiting-accept-seller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-accept-seller',
+  templateUrl: './waiting-accept-seller.component.html',
+  styleUrls: ['./waiting-accept-seller.component.css']
+})
+export class WaitingAcceptSellerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
